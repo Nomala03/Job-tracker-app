82078b41ce7025e7e3a03f00da6212df036090e8
nomala03 <mbalimadonsela03@gmail.com>
2025-08-08 19:48:56 +0200

    Job & HomePage
    created a type and interface for jobs.ts. added a home page to filter, search and sort from URL params

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import HomePage from './pages/HomePage'
 import './App.css'
 
 function App() {
@@ -18,6 +18,8 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/home" element={<HomePage />} />
+            
           </Routes>
         </div>  
       </Router> 
