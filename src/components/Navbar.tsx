fd92a2668aed5f42ba13a3c7e7a707c4f5bb84fa
nomala03 <mbalimadonsela03@gmail.com>
2025-08-10 00:23:47 +0200

    add style on Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,17 +10,17 @@ const Navbar = () => {
     };
 
     return (
-      <nav className=" bg-gray-800 text-white px-6 py-4 flex justify-between items-center">
+      <nav className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white px-6 py-4 flex justify-between items-center">
         <Link to="/" className="text-xl font-bold px-4">
           Job Tracker
         </Link> 
           <div className="flex items-center gap-4">
-          <Link to="/home" className="hover:text-gray-300">Home</Link>
-          <Link to="/add-job" className="hover:text-gray-300">Add Job</Link>
+          <Link to="/home" className="hover:underline text-white text-xl">Home</Link>
+          <Link to="/add-job" className="hover:underline text-white text-xl">Add Job</Link>
         
           <button
             onClick={handleLogout}
-            className="bg-gray-600 px-3 py-1 rounded hover:bg-gray-500"
+            className="bg-white text-purple-600 px-4 py-2 rounded-full hover:bg-gray-200 transition"
           >
             Logout
           </button>
