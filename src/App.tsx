84103191c0e7b4301e1c13da1d28b6f33e349572
nomala03 <mbalimadonsela03@gmail.com>
2025-08-11 10:28:15 +0200

    removed ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import ProtectedRoute from './components/ProtectedRoute'
-import LandingPage from './pages/LandingPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
-import JobDetailsPage from './pages/JobDetailsPage'
-import NotFoundPage from './pages/NotFoundPage'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import JobDetailsPage from "./pages/JobDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import "./App.css";
 
 function App() {
-
   return (
-      <Router>
-        <div className="min-h-screen bg-gradient-to-r text-gray-900">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>}/>
-            <Route path="/jobs/:id" element={<ProtectedRoute><JobDetailsPage /></ProtectedRoute>}/>
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>  
-      </Router> 
-  )
+    <Router>
+      <div className="min-h-screen bg-gradient-to-r text-gray-900">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/jobs/:id" element={<JobDetailsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
